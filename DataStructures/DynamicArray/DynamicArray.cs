96b28c5b6f288f62e1b3be50a4cdc01651afacbe
Anastasiya Surkova <surkova.a.i@list.ru>
2022-06-14 10:42:26 +0200

    DynamicArray// List

---
+++ DataStructures/DynamicArray/DynamicArray.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace DataStructures.DynamicArray
+{
+    public class DynamicArray<T> // List<T>
+    {
+            private const int DEFAULT_COUNT_ELEMENTS = 4;//элементы подсчета по умолчанию
+
+            public int Count
+            {
+                get
+                {
+                    return _nextIndex;
+                }
+            }
+            private T[] _elements;
+            private int _nextIndex;
+    
+
+            public DynamicArray()
+            {
+                _elements = new T[DEFAULT_COUNT_ELEMENTS];// будет создаваться массив под 4 элемента
+            }
+
+            public void Add(T value)//метод добавления элемента
+            {
+                if (_nextIndex == _elements.Length)// если _nextIndex стал указываеть на элемент, индекс которого не существует,
+                    // т.е. индекс которого равен длине массива
+                {
+                    Array.Resize(ref _elements, _elements.Length * 2);//увеличиваем размер массива
+                }
+                _elements[_nextIndex] = value;
+                _nextIndex++;
+            }
+
+            public T Get(int index)// метод получения элемента
+            {
+                return _elements[index];
+            }
+
+            public void Delete(int index)//метод удаления элемента
+            {
+                if (index < Count && index > 0)
+                {
+                    for (int i = index; i < Count; i++)
+                    {
+                        _elements[i] = _elements[i + 1];
+                    }
+
+                    _nextIndex--;
+                }
+            }
+        }
+    }
--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace DataStructures
+﻿using System;
+using DataStructures.DynamicArray;
+
+namespace DataStructures
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            DynamicArray<int> list = new DynamicArray<int>();
+            list.Add(10);
+            list.Add(20);
+            list.Add(30);
+            list.Add(40);
+            list.Add(50);
+            list.Add(60);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list.Get(i));
+            }
+            Console.WriteLine("_______________");
             
+            list.Delete(2);
+            
+            for (int i = 0; i < list.Count; i++)
+              {
+                Console.WriteLine(list.Get(i));
+              }
         }
     }
 }
+
+
+
